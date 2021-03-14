d3ae5bf14ca0f6625be7893e0bef30fcbec707a0
Marcos Carvalho <bcmarcos93@gmail.com>
2021-03-13 21:54:07 -0300

    Initial Commit

---
+++ Empregados/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Empregados
+//
+//  Created by Marcos Carvalho on 13/03/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
